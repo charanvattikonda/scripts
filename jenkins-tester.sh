b5e390e4ea8d9dfc181de17066ede4e7329e7534
cvattik <cvattikonda@walmartlabs.com>
2017-07-09 01:31:34 -0700

    adding new jenkins tester

---
+++ jenkins-tester.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo '#######################################################'
+
+echo 'This is a jenkins test-scrpit'
+
+echo ${workspace}
+
+echo `whoami`
+
+echo `pwd`
+
+echo '#######################################################'
+
+
+
